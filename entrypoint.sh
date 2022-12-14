068c5fc855da6c48749045ad8806bc91c2df4aff
Kerren Ortlepp <kerren@entrostat.com>
2022-12-14 09:46:40 +0200

    Use the variables to pass on the branch details

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,9 @@ which git-auto-merger
 #	git config --global --add safe.directory /github/workspace
 git config --global --add safe.directory $(pwd)
 
-set -x
-
 INCLUDE_PATTERN_ENTRIES=$(node /scripts/convert-array-to-args.js $2 --include-pattern)
 EXCLUDE_PATTERN_ENTRIES=$(node /scripts/convert-array-to-args.js $3 --exclude-pattern)
 
-echo $INCLUDE_PATTERN_ENTRIES
-echo $EXCLUDE_PATTERN_ENTRIES
+set -x
 
-git-auto-merger merge --base-branch=develop --include-pattern='develop$' --include-pattern='feature\/.*' --exclude-pattern='main'
+git-auto-merger merge --base-branch=develop --include-pattern=$1 $INCLUDE_PATTERN_ENTRIES $EXCLUDE_PATTERN_ENTRIES
