1c01c676c282f85597c1f02cda7c969348e6a37a
Kerren Ortlepp <kerren@entrostat.com>
2022-12-14 09:36:11 +0200

    Use a try catch to allow for an array or a single input

--- convert-array-to-args.js
+++ convert-array-to-args.js
@@ -1,6 +1,9 @@
 const arrayOfInputs = process.argv[2];
 const argumentOption = process.argv[3];
 
-const arr = JSON.parse(arrayOfInputs);
-
-console.log(arr.map(input => `${argumentOption}=${input}`).join(' '));
+try {
+    const arr = JSON.parse(arrayOfInputs);
+    console.log(arr.map(input => `${argumentOption}=${input}`).join(' '));
+} catch (e) {
+    console.log(`${argumentOption}=${arrayOfInputs}`)
+}
