6b807ae8bad67d3c0cbae63610953faf877c592f
Kerren Ortlepp <kerren@entrostat.com>
2022-12-14 11:01:24 +0200

    Added the SMTP details to the action for testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,5 +14,16 @@ git pull --all
 
 INCLUDE_PATTERN_ENTRIES=$(node /scripts/convert-array-to-args.js $2 --include-pattern)
 EXCLUDE_PATTERN_ENTRIES=$(node /scripts/convert-array-to-args.js $3 --exclude-pattern)
+NOTIFY_EMAILS=$(node /scripts/convert-array-to-args.js $4 --notify-email)
 
-git-auto-merger merge --base-branch=$1 ${INCLUDE_PATTERN_ENTRIES} ${EXCLUDE_PATTERN_ENTRIES}
+USE_TLS=$9
+if [[ "$USE_TLS" = "1" || "$USE_TLS" = "true" || "$USE_TLS" = "TRUE"]]
+then
+  TLS_FLAG="--tls"
+else
+  TLS_FLAG=""
+fi
+
+set:credentials:smtp --host=$5 --port=$6 --username=$7 --password=$8 ${TLS_FLAG} --from-address=$10
+
+git-auto-merger merge --base-branch=$1 ${INCLUDE_PATTERN_ENTRIES} ${EXCLUDE_PATTERN_ENTRIES} ${NOTIFY_EMAILS}
