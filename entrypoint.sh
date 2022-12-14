676f127dac8cc50f55669eb67a8c5de7fae3be31
Kerren Ortlepp <kerren@entrostat.com>
2022-12-14 10:03:17 +0200

    Removed the flags code

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,4 @@ git pull --all
 INCLUDE_PATTERN_ENTRIES=$(node /scripts/convert-array-to-args.js $2 --include-pattern)
 EXCLUDE_PATTERN_ENTRIES=$(node /scripts/convert-array-to-args.js $3 --exclude-pattern)
 
-set -x
-
-git-auto-merger merge --base-branch=$1 "${INCLUDE_PATTERN_ENTRIES[@]}" "${EXCLUDE_PATTERN_ENTRIES[@]}"
+git-auto-merger merge --base-branch=$1 ${INCLUDE_PATTERN_ENTRIES} ${EXCLUDE_PATTERN_ENTRIES}
