84be8f6ce8df65c66d2ad23859b8a377fe6567d5
Kerren Ortlepp <kerren@entrostat.com>
2022-12-14 09:10:04 +0200

    Added the fix for dubious ownership

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,11 @@
 
 which git-auto-merger
 
+# This is to fix the following error
+# fatal: detected dubious ownership in repository at '/github/workspace'
+# To add an exception for this directory, call:
+#
+#	git config --global --add safe.directory /github/workspace
+git config --global --add safe.directory $(pwd)
+
 git-auto-merger merge --base-branch=develop --include-pattern='develop$' --include-pattern='feature\/.*' --exclude-pattern='main'
