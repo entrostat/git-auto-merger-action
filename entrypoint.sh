b1ffb900dab7704cbe6f748560a0834d7f059942
Kerren Ortlepp <kerren@entrostat.com>
2022-12-27 16:11:54 +0200

    Added the flag to push the commits

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ which git-auto-merger
 #
 #	git config --global --add safe.directory /github/workspace
 git config --global --add safe.directory $(pwd)
-git config --global user.email "git-auto-merger@git-auto-merger.com"
+git config --global user.email "git-auto-merger@users.noreply.github.com"
 git config --global user.name "git-auto-merger"
 
 git fetch --all
@@ -19,7 +19,7 @@ EXCLUDE_PATTERN_ENTRIES=$(node /scripts/convert-array-to-args.js $3 --exclude-pa
 NOTIFY_EMAILS=$(node /scripts/convert-array-to-args.js $4 --notify-email)
 
 USE_TLS=$9
-if [[ "$USE_TLS" == "1" || "$USE_TLS" == "true" || "$USE_TLS" == "TRUE" ]];
+if [[ "$USE_TLS" == "1" || "$USE_TLS" == "true" || "$USE_TLS" == "TRUE" || "$USE_TLS" == "True" ]];
 then
   TLS_FLAG="--tls"
 else
@@ -28,4 +28,13 @@ fi
 
 git-auto-merger set:credentials:smtp --host=$5 --port=$6 --username=$7 --password=$8 ${TLS_FLAG} --from=${10}
 
-git-auto-merger merge --base-branch=$1 ${INCLUDE_PATTERN_ENTRIES} ${EXCLUDE_PATTERN_ENTRIES} ${NOTIFY_EMAILS} --project-name="${11}"
+SKIP_MERGE=${12}
+if [[ "$SKIP_MERGE" == "1" || "$SKIP_MERGE" == "true" || "$SKIP_MERGE" == "TRUE" || "$SKIP_MERGE" == "True" ]];
+then
+  echo "Skipping the merge push..."
+  PUSH_COMMIT=""
+else
+  PUSH_COMMIT="--push-commit"
+fi
+
+git-auto-merger merge --base-branch=$1 ${INCLUDE_PATTERN_ENTRIES} ${EXCLUDE_PATTERN_ENTRIES} ${NOTIFY_EMAILS} --project-name="${11}" ${PUSH_COMMIT}
